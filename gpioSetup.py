0af4e61dfca9f7cfab99a36cbe50bf2113050e95
gitParker <ryanlauraparker@gmail.com>
2017-11-08 21:28:26 -0600

    testing temp sensor

--- gpioSetup.py
+++ gpioSetup.py
@@ -3,12 +3,14 @@ import RPi.GPIO as gpio
 PIN_HEAT_A = 7
 PIN_HEAT_B = 11
 PIN_HEAT_ON = 13
+PIN_TEMP_IN = 15
 
 gpio.setwarnings(False)
 gpio.setmode(gpio.BOARD)
 gpio.setup(PIN_HEAT_A, gpio.OUT)
 gpio.setup(PIN_HEAT_B, gpio.OUT)
 #gpio.setup(PIN_HEAT_ON, gpio.IN)
+gpio.setup(PIN_TEMP_IN, gpio.IN)
 
 gpio.setup(PIN_HEAT_ON, gpio.IN, pull_up_down=gpio.PUD_UP)
 #gpio.add_event_detect(PIN_HEAT_ON, gpio.BOTH, callback=toggle_callback, bouncetime=300)
